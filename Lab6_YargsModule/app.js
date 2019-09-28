3d47ee771a5adfb12802237e40e4a3f46c05e4c9
madhurisarda <54335408+madhurisarda@users.noreply.github.com>
2019-09-28 13:56:57 -0500

    lab6

---
+++ Lab6_YargsModule/app.js
@@ -0,0 +1,100 @@
+
+const fs =  require('fs');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+// ------------ Begin - command configuration -----------------
+const customerID = {
+    describe: 'ID of Customer',
+    demand : true,
+    alias : 'id'
+}
+
+const customerName = {
+    describe: 'Name of Customer',
+    demand : true,
+    alias : 'name'
+}
+
+const customerEmail = {
+    describe: 'Email ID of Customer',
+    demand : true,
+    alias : 'email'
+}
+
+const argv =  yargs
+
+    .command('add','Add a new note',{
+        ID: customerID,
+        Name: customerName,
+        Email: customerEmail
+    })
+    .command('update','Update an existing note',{
+        ID: customerID,
+        Name: customerName,
+        Email: customerEmail
+    })
+    .command('list','List all notes')
+    .command('read','Read a note',{
+        ID: customerID,
+        Name: customerName,
+        Email: customerEmail
+    })
+    .command('remove','Remove a Note',{
+      ID: customerID
+    })
+    .help()
+    .argv;
+
+
+// ------------ End - command configuration -----------------
+
+
+var command = yargs.argv._[0];
+
+
+if (command === 'add'){
+    var note = notes.addNote(argv.ID,argv.Name, argv.Email);
+    if (note){
+      notes.logNote(note);                                //add a new note
+    } else{
+      console.log("Note was updated");
+    }
+}
+
+else if (command === 'update'){
+    var note = notes.updateNotes(argv.ID,argv.Name, argv.Email);
+    if (note){
+        notes.logNote(note);                                //add a new note
+    } else{
+        console.log("Note was updated");
+    }
+}
+
+else if (command === 'list') {
+  var AllNotes = notes.getAll();
+  console.log(`Printing ${AllNotes.length} note(s).`);
+  AllNotes.forEach((note)=>{                                //list all note(s)
+    notes.logNote(note);
+  });
+}
+
+else if (command === 'read') {
+   var note = notes.getNote(argv.ID);
+   if(note){
+    notes.logNote(note);                                //read a note 
+          }
+   else{
+    console.log("Note not found");
+   }
+}
+else if (command === 'remove') {
+    var noteRemoved = notes.remove(argv.ID);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
+}
+
+else{
+  console.log('command note recognized'); 
+}
---
+++ Lab6_YargsModule/notes.js
@@ -0,0 +1,105 @@
+const fs =  require('fs');
+
+
+// ------------------Begin of Reusable functions ---------------------
+
+var fetchNotes = () => {
+  try {                          //if file won't exist
+    var notesString = fs.readFileSync('notes-data.json')
+    return JSON.parse(notesString);
+  } catch(e){
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+};
+
+
+// ------------------End of Reusable functions ---------------------
+
+var updateNotes = (ID,Name,Email) => {
+    remove(ID); // update the value for existing notes
+    var customerNote = {ID,Name,Email}
+    var filteredNotes = fetchNotes();
+    filteredNotes.push(customerNote);
+    saveNotes(filteredNotes);
+    return customerNote
+}
+//  to add a new note
+
+var addNote = (ID,Name,Email) => {
+    var notes = fetchNotes();
+    var customerNote = {ID,Name,Email}
+    var duplicateNotes =  notes.filter((note) => { // to check if note already exists
+       if(note.ID === ID) {
+           updateNotes(ID,Name,Email);
+           /*remove(ID); // update the value for existing notes
+           var filteredNotes = fetchNotes();
+           filteredNotes.push(customerNote);
+           saveNotes(filteredNotes);*/
+       }
+        return note.ID === ID;
+    });
+    if (duplicateNotes.length === 0){
+      notes.push(customerNote);
+      saveNotes(notes);
+      return customerNote
+    }
+
+  };
+
+
+//to list all the notes
+
+var getAll = () => {
+    return fetchNotes();
+};
+
+
+// to read a note
+
+var getNote = (ID) => {
+    
+    var notes = fetchNotes();
+
+    var getNotes =  notes.filter((note) => {  // to check if note exists and return note
+      return note.ID === ID;
+    });
+
+    return getNotes[0]
+
+};
+
+
+// to delete a note
+
+var remove = (ID) => {
+
+    var notes = fetchNotes(); // reusable func
+
+    var filteredNotes =  notes.filter((note) => { // will return all other notes other than "note to be removed"
+      return note.ID !== ID;
+    });
+
+    saveNotes(filteredNotes); //save new notes array
+
+    return notes.length !== filteredNotes.length
+    
+};
+
+// function just to print out note to screen
+
+var logNote = (note) => {
+    console.log('--');
+    console.log(`Customer ID: ${note.ID}`);
+    console.log(`Customer Name: ${note.Name}`);
+    console.log(`Customer Email: ${note.Email}`);
+};
+
+// add new function names here to be accessible from other modules
+
+module.exports = {
+  addNote, getAll, remove, getNote,logNote, updateNotes
+};
